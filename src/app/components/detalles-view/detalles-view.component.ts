dfabaae6462662297f4d91b9732a73365e86459c
ESPACEX88 <pos191102@uvg.edu.gt>
2023-09-19 12:27:18 -0600

    Implementación de buscador detalle-view

--- src/app/components/detalles-view/detalles-view.component.ts
+++ src/app/components/detalles-view/detalles-view.component.ts
@@ -7,12 +7,13 @@ import { UserRegister } from 'src/app/services/user.service';
   styleUrls: ['./detalles-view.component.css']
 })
 export class DetallesViewComponent implements OnInit {
-  userCollectionData: any[] = []; // Aquí almacenarás los datos de la colección "Calculos"
-  currentIndex: number = 0; // Variable para el índice actual
+  userCollectionData: any[] = [];
+  searchQuery: string = ''; // Variable para almacenar la consulta de búsqueda
+  currentIndex: number = 0;
+
   constructor(private userRegister: UserRegister) {}
 
   ngOnInit(): void {
-    // Llama al servicio para obtener los datos de la colección "Calculos"
     this.userRegister.getAuthenticateUserCollection().then((data) => {
       this.userCollectionData = data;
     });
