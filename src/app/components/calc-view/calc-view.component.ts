babc9de351e80fbbafc29b10955b9f73b1a0181a
Mailo59 <mor21804@uvg.edu.gt>
2023-09-19 02:43:40 -0600

    Se añadio la funcion del calculo local

--- src/app/components/calc-view/calc-view.component.ts
+++ src/app/components/calc-view/calc-view.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { UserRegister } from 'src/app/services/user.service';
+import { UserProfileInfoService } from 'src/app/services/user-profile-info.service';
+import { Router } from '@angular/router';
+import { UserCredential } from 'firebase/auth';
 
 @Component({
   selector: 'app-calc-view',
@@ -6,5 +11,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./calc-view.component.css']
 })
 export class CalcViewComponent {
+  formCalc: FormGroup;
+  public errorFire = '';
   titulo: string = 'Calculadora de Intereses';
+
+  constructor(
+    private userService: UserRegister,
+    private router: Router,
+    private userProfileInfo: UserProfileInfoService,
+  ) {
+    this.formCalc = new FormGroup({
+      Capital: new FormControl(),
+      Cuotas: new FormControl(),
+      Tasa: new FormControl(),
+    });
+  }
+
+// Funcion que calcula el interes simple
+calcular() {
+  // Se obtienen los valores del formulario
+  const capital = this.formCalc.get('Capital')?.value;
+  const cuotas = this.formCalc.get('Cuotas')?.value;
+  const tasa = this.formCalc.get('Tasa')?.value;
+
+  // Calculo del interés simple
+  const interes = capital*(tasa/ 100)*cuotas;
+
+  // Imprime los valores en la consola
+  console.log('Capital:', capital);
+  console.log('Cuotas:', cuotas);
+  console.log('Tasa de Interés:', tasa);
+  console.log('Interés:', interes);
+}
 }
