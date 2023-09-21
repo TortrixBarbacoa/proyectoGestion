11a81ac6d1f4a04539260f7308b20c3a97e9ea7c
ESPACEX88 <pos191102@uvg.edu.gt>
2023-09-21 11:12:23 -0600

    implementación PDF

--- src/app/components/detalles-view/detalles-view.component.ts
+++ src/app/components/detalles-view/detalles-view.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserProfileInfoService } from 'src/app/services/user-profile-info.service';
 import { UserRegister } from 'src/app/services/user.service';
+import { PDFDocument, rgb} from 'pdf-lib';
+
+
 
 @Component({
   selector: 'app-detalles-view',
@@ -8,7 +11,8 @@ import { UserRegister } from 'src/app/services/user.service';
   styleUrls: ['./detalles-view.component.css']
 })
 export class DetallesViewComponent implements OnInit {
-  
+  editCalcCuotas: any;
+  editCalcCantidad:any;
   collecionAllUsers: any[] = [];
   editCalculationData: any = null; // Almacena los datos de edición
   editCalculationIndex: number = -1; // Almacena el índice del préstamo que se está editando (-1 para indicar que no se está editando)
@@ -52,18 +56,23 @@ export class DetallesViewComponent implements OnInit {
   }
 
 openEditModal(calculo: any) {
-  this.editCalcId= calculo.id; 
-  this.editCalcAlias = calculo.alias;
+  this.editCalcId = calculo.id; 
   this.editCalcMonthly = calculo.monthly;
+  this.editCalcCantidad = calculo.cantidad;
+  this.editCalcAlias = calculo.alias;
   this.editCalcInteres = calculo.interes;
-  this.editCalcTotalInteres = calculo.totalInteres;
+ this.editCalcCuotas = calculo.cuotas;
+this.editCalcTotalInteres = calculo.totalInteres;
 
-}
+  }
+
+
 
 editCalculation() {
   // Crear un objeto con los datos actualizados
   const updatedCalculation = {
     alias: this.editCalcAlias,
+    cantidad:this.editCalcCantidad ,
     monthly: this.editCalcMonthly,
     interes: this.editCalcInteres,
     totalInteres: this.editCalcTotalInteres,
@@ -110,7 +119,252 @@ deleteCalculationById(userId: string, calculationId: string) {
     });
 }
 
-// ...
+
+async generarPDF() {
+
+  // Crear un nuevo documento PDF
+ const pdfDoc = await PDFDocument.create();
+
+ // Agregar una nueva página al documento
+ const page = pdfDoc.addPage([612, 792]);
+
+ // Definir las posiciones y dimensiones de elementos en la página
+ const xPosition = 200;
+ const yPosition = 650;
+ const xPositio = 70;
+ const yPositio = 640;
+ const columnWidth = 90;
+ const rowHeight = 20;
+ const fontSize = 12;
+ const fontSize1 = 10;
+ const fontSized = 10;
+
+ // Cargar y embeber la primera imagen en el documento PDF
+ const imageBytes1 = await fetch('../../../assets/img/Login-Side.png').then((response) =>
+   response.arrayBuffer()
+ );
+ const image1 = await pdfDoc.embedPng(imageBytes1);
+
+ // Dibujar la primera imagen en la esquina superior izquierda
+ const imageWidth1 = 120;
+ const imageHeight1 = 75;
+ page.drawImage(image1, {
+   x: 0,
+   y: page.getHeight() - imageHeight1,
+   width: imageWidth1,
+   height: imageHeight1,
+ });
+
+ // Cargar y embeber la segunda imagen en el documento PDF
+ const imageBytes2 = await fetch('../../../assets/img/titulo.png').then((response) =>
+   response.arrayBuffer()
+ );
+ const image2 = await pdfDoc.embedPng(imageBytes2);
+
+ // Dibujar la segunda imagen centrada en la parte superior de la página
+ const imageWidth2 = 263;
+ const imageHeight2 = 50;
+ const centerX2 = page.getWidth() / 2 - imageWidth2 / 1.8;
+ const centerY2 = page.getHeight() - imageHeight1 - 1;
+ page.drawImage(image2, {
+   x: centerX2,
+   y: centerY2,
+   width: imageWidth2,
+   height: imageHeight2,
+ });
+
+// Define el texto que deseas agregar
+const text = "Guatemala, Guatemala";
+const text1 = "4184-7989";
+const text2 = "GestordePrestamos@gmail.com";
+
+// Definir el margen superior y derecho para centrar el texto
+const marginRight = 130;
+const marginTop = page.getHeight() - 30;
+
+// Agregar el texto a la página justificado a la derecha
+page.drawText(text, {
+x: page.getWidth() - marginRight,
+y: marginTop,
+size: fontSize,
+maxWidth: page.getWidth() - 2 * marginRight, // Ancho máximo para justificar
+font: await pdfDoc.embedFont('Times-Roman'),
+});
+
+// Cargar la imagen del icono
+const iconImageBytes = await fetch('../../../assets/img/Icon-location.png').then((response) =>
+response.arrayBuffer()
+);
+
+// Superponer el icono junto al texto
+const iconImage = await pdfDoc.embedPng(iconImageBytes);
+const iconWidth = 20;
+const iconHeight = 20;
+page.drawImage(iconImage, {
+x: page.getWidth() - marginRight - iconWidth - 5,
+y: marginTop - (fontSize / 2),
+width: iconWidth,
+height: iconHeight,
+});
+
+// Definir el margen superior y derecho para centrar el texto
+const marginRight1 = 72;
+const marginTop1 = page.getHeight() - 48;
+
+// Agregar el texto a la página justificado a la derecha
+page.drawText(text1, {
+x: page.getWidth() - marginRight1,
+y: marginTop1,
+size: fontSize,
+maxWidth: page.getWidth() - 2 * marginRight1,
+font: await pdfDoc.embedFont('Times-Roman'),
+});
+
+// Cargar la imagen del icono
+const iconImageBytes2 = await fetch('../../../assets/img/telephone.png').then((response) =>
+response.arrayBuffer()
+);
+
+// Superponer el segundo icono junto al segundo texto
+const iconImage2 = await pdfDoc.embedPng(iconImageBytes2);
+const iconWidth2 = 15;
+const iconHeight2 = 15;
+page.drawImage(iconImage2, {
+x: page.getWidth() - marginRight1 - iconWidth2 - 5,
+y: marginTop1 - (fontSize / 2),
+width: iconWidth2,
+height: iconHeight2,
+});
+
+// Definir el margen superior y derecho para centrar el texto
+const marginRight3 = 150;
+const marginTop3 = page.getHeight() - 64;
+
+// Agregar el texto a la página justificado a la derecha
+page.drawText(text2, {
+x: page.getWidth() - marginRight3,
+y: marginTop3,
+size: fontSize1,
+maxWidth: page.getWidth() - 2 * marginRight3,
+font: await pdfDoc.embedFont('Times-Roman'),
+});
+
+// Cargar la imagen del icono
+const iconImageBytes3 = await fetch('../../../assets/img/correo.png').then((response) =>
+response.arrayBuffer()
+);
+
+// Superponer el icono junto al texto
+const iconImage3 = await pdfDoc.embedPng(iconImageBytes3);
+const iconWidth3 = 15;
+const iconHeight3 = 12;
+page.drawImage(iconImage3, {
+x: page.getWidth() - marginRight3 - iconWidth3 - 5,
+y: marginTop3 - (fontSize / 2),
+width: iconWidth3,
+height: iconHeight3,
+});
+
+ // Dibuja una línea de separación después de las imágenes
+ const lineY = centerY2 - 10;
+ const lineWidth = page.getWidth() - 20;
+ const lineThickness = 1.5;
+ page.drawLine({
+   start: { x: 10, y: lineY },
+   end: { x: lineWidth, y: lineY },
+   thickness: lineThickness,
+ });
+
+ // Dibujar el título en la página
+ page.drawText('DETALLES DE CALCULOS', {
+   x: xPosition,
+   y: yPosition,
+   size: 16,
+   font: await pdfDoc.embedFont('Helvetica-Bold'),
+ });
+
+  // Definir los encabezados de la tabla
+  const headers = ['Detalle', 'Cantidad', 'No.Cuotas', '% Interés', 'Mensualidad', 'Total Intereses'];
+
+// Definir los datos de la tabla
+const data = this.userCollectionData.map((calculo) => [
+  calculo.alias,
+  calculo.cantidad.toString(), // Convertir a string
+  calculo.cuotas.toString(),   // Convertir a string
+  calculo.interes.toString(), // Convertir a string
+  calculo.monthly.toString(),  // Convertir a string
+  calculo.totalInteres.toString(), // Convertir a string
+]);
+
+
+ let currentY = yPositio - 30;
+
+ // Dibuja los encabezados de la tabla
+ for (let i = 0; i < headers.length; i++) {
+   page.drawText(headers[i], {
+     x: xPositio + i * columnWidth,
+     y: currentY,
+     size: fontSize,
+     font: await pdfDoc.embedFont('Helvetica-Bold'),
+   });
+ }
+
+ // Aumenta la posición Y para empezar a dibujar los datos
+ currentY -= rowHeight;
+
+ // Dibuja los datos de la tabla
+ for (const row of data) {
+   for (let i = 0; i < row.length; i++) {
+     page.drawText(row[i], {
+       x: xPositio + i * columnWidth,
+       y: currentY,
+       size: fontSized,
+     });
+   }
+   currentY -= rowHeight; // Desplaza la posición Y para la siguiente fila
+ }
+
+ // Dibujar una línea de división antes del pie de página
+ const divisionLineY = 50;
+ const divisionLineWidth = page.getWidth() - 210;
+ const divisionLineThickness = 1.5;
+ page.drawLine({
+   start: { x: 10, y: divisionLineY },
+   end: { x: 190 + divisionLineWidth, y: divisionLineY },
+   thickness: divisionLineThickness,
+ });
+
+ // Agregar un pie de página en la misma página
+ const footerText = 'Para más información o cualquier problema o inquietud, envíenos un correo electronico';
+ const footerText1 = 'GestordePrestamos@gmail.com';
+
+ // Dibujar el texto del pie de página
+ page.drawText(footerText, {
+   x: 98,
+   y: 30,
+   size: 12,
+   font: await pdfDoc.embedFont('Times-Roman'),
+ });
+
+ // Dibujar el texto del pie de página
+ page.drawText(footerText1, {
+   x: 210,
+   y: 20,
+   size: 12,
+   font: await pdfDoc.embedFont('Times-Bold'),
+ });
+
+
+
+ // Guardar el documento PDF generado
+ const pdfBytes = await pdfDoc.save();
+ const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+ const url = URL.createObjectURL(blob);
+
+ // Abrir el PDF en una nueva ventana
+ window.open(url);
+}
+
 
 
 }
